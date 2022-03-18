396e41669f56bc0f4b8720230f0516cd49bf11a3
askininan <askin.inan@outlook.com>
2022-03-18 18:57:45 +0300

    client side of the app is finished

--- server.js
+++ server.js
@@ -27,4 +27,12 @@ app.get('/',(req,res)=>{
     res.render('index');
 })
 
+app.get('/add_user',(req,res)=>{
+    res.render('add_user');
+})
+
+app.get('/update_user',(req,res)=>{
+    res.render('update_user');
+})
+
 app.listen(PORT,()=>{console.log('Server is runnning on http://localhost:${PORT}')});
