04101983f8f545d36f11e747097e017cd104f2db
askininan <askin.inan@outlook.com>
2022-03-19 18:30:02 +0300

    Finished find method on controller

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -29,20 +29,39 @@ exports.create = (req,res) =>{
         })
     }
 
+
 // retrieve and return all users/ retrieve and return a signle user
 exports.find = (req,res)=>{
     Userdb.find()
     .then(user => {
         res.send(user)
     })
-    .catch(err=>{
-        res.status(500).send({message : err.message || "Error occurred while retrieving user info"})
+    .catch(err => {
+        res.status(500).send({ message : err.message || "Error Occurred while retriving user information" })
     })
 }
 
+
 // Update a new idetified user by user id
 exports.update = (req,res)=>{
+    if(!req.body){
+        return res
+            .status(400)
+            .send({message: "Data to update cannot be empty"})
+    }
 
+    const id = req.params.id;
+    Userdb.findByIdAndUpdate(id,req.body, {useFindandModify:false})
+        .then(data => {
+            if(!data){
+                res.status(404).send({message:"Cannot Update user with " + id + " User not found"})
+            }else{
+                res.send(data)
+            }
+        })
+        .catch(err =>{
+            res.status(500).send({message:"Error Update user info"})
+        })
 }
 
 // Delete a user with a specified user id in the request
--- server/routes/router.js
+++ server/routes/router.js
@@ -22,7 +22,10 @@ route.get('/add_user', services.add_user)
  */
 route.get('/update_user', services.update_user)
 
-//API
-route.post('/api/users',controller.create);
+// API
+route.post('/api/users', controller.create);
+route.get('/api/users', controller.find);
+route.put('/api/users/:id', controller.update);
+route.delete('/api/users/:id', controller.delete);
 
 module.exports = route
