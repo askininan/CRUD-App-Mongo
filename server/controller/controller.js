74ffe840cd2c9f9de07f00b1a8754c2530ab0ac0
askininan <askin.inan@outlook.com>
2022-03-19 18:07:31 +0300

    Finished create user API and tested with Postman

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -2,21 +2,50 @@ var Userdb = require('../model/model');
 
 //create and save new user API
 exports.create = (req,res) =>{
+    // validate request
+    if(!req.body){
+        res.status(400).send({message: "Content can not be empty!"});
+        return;
+    }
 
-}
+    // new user instance
+    const user = new Userdb({
+        name:req.body.name,
+        surname:req.body.surname,
+        email:req.body.email,
+        status:req.body.status
+    })
 
-// retreşeve and return all users/ retrieve and return a signle user
+    // Save user in the database
+    user
+        .save(user)
+        .then(data=>{
+            res.send(data)
+        })
+        .catch(err=>{
+            res.status(500).send({
+                message:err.message || "An error occured while creating a user"
+            })
+        })
+    }
 
+// retrieve and return all users/ retrieve and return a signle user
 exports.find = (req,res)=>{
-
+    Userdb.find()
+    .then(user => {
+        res.send(user)
+    })
+    .catch(err=>{
+        res.status(500).send({message : err.message || "Error occurred while retrieving user info"})
+    })
 }
 
-//Update a new idetified user by user id
+// Update a new idetified user by user id
 exports.update = (req,res)=>{
 
 }
 
 // Delete a user with a specified user id in the request
 exports.delete = (req,res)=>{
-    
+
 }
