09f123acb7ab08fabcf99a43dca7dde2a1eb7a19
askininan <askin.inan@outlook.com>
2022-03-20 15:25:13 +0300

    Finished API calls on index.js

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -20,7 +20,7 @@ exports.create = (req,res) =>{
     user
         .save(user)
         .then(data=>{
-            res.send(data)
+            res.redirect('/add_user')
         })
         .catch(err=>{
             res.status(500).send({
